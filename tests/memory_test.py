76330c6c182a12be9944f43ce5388ce516c12885
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-08-22 20:49:07 +0200

    feat(tests/memory): add revnet_uses_more_without_savings_test

--- tests/memory_test.py
+++ tests/memory_test.py
@@ -1,3 +1,6 @@
+import copy
+import typing
+
 import numpy as np
 import pytest
 import torch
@@ -30,25 +33,38 @@ def block():
 inp = torch.randn((1, 3, 224, 224)).cuda()
 
 
-def run(model: torch.nn.Module):
+def run(model: torch.nn.Module) -> float:
     _unused = model(inp)
     return torch.cuda.memory_allocated() * 2 ** -20
 
 
 @pytest.mark.parametrize("depth", [16, 64])
 def revnet_uses_less_test(depth: int):
-    blocks = [block() for _ in range(depth)]
+    blocks = [block()] * depth
     rev = nn.Sequential(conv(3, 2 * channels), revlib.ReversibleSequential(*blocks), conv(2 * channels, classes)).cuda()
     seq = nn.Sequential(conv(3, channels), *blocks, conv(channels, classes)).cuda()
     assert run(rev) < run(seq)
 
 
-@pytest.mark.parametrize("depth", [16, 64])
-@pytest.mark.parametrize("factor", [2, 4, 32])
-def no_more_memory_with_depth_test(depth: int, factor: int):
-    blocks = [block() for _ in range(depth)]
+def double_rev(stem0, stem1) -> typing.Tuple[float, float]:
     inp_mod = conv(3, 2 * channels)
     out_mod = conv(2 * channels, classes)
-    rev0 = nn.Sequential(inp_mod, revlib.ReversibleSequential(*blocks), out_mod).cuda()
-    rev1 = nn.Sequential(inp_mod, revlib.ReversibleSequential(*(blocks * factor)), out_mod).cuda()
-    assert np.isclose(run(rev0), run(rev1), rtol=0.1)
+    rev0 = nn.Sequential(inp_mod, stem0, out_mod).cuda()
+    rev1 = nn.Sequential(inp_mod, stem1, out_mod).cuda()
+    return run(copy.deepcopy(rev0)), run(copy.deepcopy(rev1))
+
+
+@pytest.mark.parametrize("depth", [8, 32])
+def revnet_uses_more_without_savings_test(depth: int):
+    blocks = [block()] * depth
+    assert float.__gt__(*double_rev(revlib.ReversibleSequential(*blocks, memory_savings=False),
+                                    revlib.ReversibleSequential(*blocks, memory_savings=True)))
+
+
+@pytest.mark.parametrize("depth", [1, 4])
+@pytest.mark.parametrize("factor", [4, 16, 64])
+def no_more_memory_with_depth_test(depth: int, factor: int):
+    blocks = [block() for _ in range(depth)]
+    assert np.isclose(*double_rev(revlib.ReversibleSequential(*blocks),
+                                  revlib.ReversibleSequential(*(blocks * factor))),
+                      rtol=0.05)
