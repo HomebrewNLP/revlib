fa22303e042eb7a79ddc6105d4e047e5b723c3e2
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-02 08:56:44 +0200

    fix(backward_test): do backward pass

--- tests/backward_test.py
+++ tests/backward_test.py
@@ -13,16 +13,18 @@ class GradTest(RevTest):
         self.reductions: typing.List[typing.Callable] = [torch.mean, torch.std, torch.max, torch.min]
 
     def run(self, mod: torch.nn.Module):
-        _unused = mod(self.inp)
+        out = mod(self.inp)
+        out.mean().backward()
         p = next(iter(mod.parameters()))
-        return [r(p).item() for r in self.reductions]
+        return [r(p.grad).item() for r in self.reductions]
 
 
 base = GradTest()
 
 
-@pytest.mark.parametrize("depth", [1, 8, 64])
+@pytest.mark.parametrize("depth", [1, 8])
 @pytest.mark.parametrize("weight_sharing", [True, False])
 def same_gradients_without_memory_savings_test(depth: int, weight_sharing: bool):
     blocks = ([base.block()] * depth) if weight_sharing else [base.block() for _ in range(depth)]
     base(base.revnet(blocks, False), base.revnet(blocks, True), np.allclose)
+
