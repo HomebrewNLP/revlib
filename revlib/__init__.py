11d2934df0eedcf62bb5c618bfdb304d2cc154a3
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-09-02 08:57:13 +0200

    Revert "feat(model): allow model to be stored on cpu"
    This reverts commit 550fc9d0b927bc607466de74d0856d17dcf85636.

--- revlib/__init__.py
+++ revlib/__init__.py
@@ -1,4 +1,3 @@
-import copy
 import typing
 
 import torch
@@ -21,14 +20,11 @@ class _ReplaceGrad(torch.autograd.Function):
 class _ReversibleHalfResidualSwapFn(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x0: torch.Tensor, back_x0: torch.Tensor, x1: torch.Tensor, back_x1: torch.Tensor,
-                mod: typing.Callable, coupling_forward: typing.Callable, coupling_inverse: typing.Callable,
-                move_module_to_gpu: bool) -> QUAD_TENSOR:
+                mod: typing.Callable, coupling_forward: typing.Callable,
+                coupling_inverse: typing.Callable) -> QUAD_TENSOR:
         ctx.mod = mod
         ctx.coupling_inverse = coupling_inverse
         ctx.forward_rng_state = torch.get_rng_state()
-        ctx.move_module_to_gpu = move_module_to_gpu
-        if move_module_to_gpu:
-            mod = copy.deepcopy(ctx.mod).to(x1.device)
         return x1, back_x0, coupling_forward(x0, mod(x1)), back_x1
 
     @staticmethod
@@ -36,9 +32,6 @@ class _ReversibleHalfResidualSwapFn(torch.autograd.Function):
                  ) -> typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, None, None, None]:
         original_rng_state = torch.get_rng_state()
         torch.set_rng_state(ctx.forward_rng_state)
-        original_device = ctx.mod.device
-        if ctx.move_module_to_gpu:
-            ctx.mod.to(y0.device)
         with torch.enable_grad():
             y0 = y0.requires_grad_()
             y0.retain_grad()
@@ -47,8 +40,6 @@ class _ReversibleHalfResidualSwapFn(torch.autograd.Function):
             x0 = ctx.coupling_inverse(y1, out.detach())
         torch.autograd.backward(out, dy1)
         torch.set_rng_state(original_rng_state)
-        if ctx.move_module_to_gpu:
-            ctx.mod.to(original_device)
         with torch.enable_grad():
             return dy1.detach(), x0.detach_(), y0.grad.add_(dy0).detach_(), y0.detach_(), None, None, None
 
@@ -67,39 +58,27 @@ def additive_coupling_inverse(output: torch.Tensor, fn_out: torch.Tensor) -> tor
 
 class ReversibleModule(torch.nn.Module):
     def __init__(self, wrapped_module: torch.nn.Module, coupling_forward: typing.Optional[typing.Callable] = None,
-                 coupling_inverse: typing.Optional[typing.Callable] = None, move_module_to_gpu: bool = False,
-                 memory_savings: bool = False):
+                 coupling_inverse: typing.Optional[typing.Callable] = None):
         super(ReversibleModule, self).__init__()
         self.wrapped_module = wrapped_module
         self.coupling_forward = coupling_forward or additive_coupling_forward
         self.coupling_inverse = coupling_inverse or additive_coupling_inverse
-        self.move_module_to_gpu = move_module_to_gpu
-        self.memory_savings = memory_savings
 
     def forward(self, inp: QUAD_TENSOR) -> QUAD_TENSOR:
-        if self.memory_savings:
-            return reverse_and_swap(*inp, self.wrapped_module, self.coupling_forward, self.coupling_inverse,
-                                    self.move_module_to_gpu)
-        else:
-            x0, back_x0, x1, back_x1 = inp
-            return x1, back_x0, self.coupling_forward(x0, self.wrapped_module(x1)), back_x1
+        return reverse_and_swap(*inp, self.wrapped_module, self.coupling_forward, self.coupling_inverse)
 
 
 class ReversibleSequential(torch.nn.Module):
     def __init__(self, *modules, split_dim=1,
                  coupling_forward: typing.Optional[typing.List[typing.Optional[typing.Callable]]] = None,
-                 coupling_inverse: typing.Optional[typing.List[typing.Optional[typing.Callable]]] = None,
-                 move_module_to_gpu: bool = False,
-                 memory_savings: bool = True):
+                 coupling_inverse: typing.Optional[typing.List[typing.Optional[typing.Callable]]] = None):
         super(ReversibleSequential, self).__init__()
         coupling_forward = list(coupling_forward) if coupling_forward else [None]
         coupling_inverse = list(coupling_inverse) if coupling_inverse else [None]
         self.stem = torch.nn.Sequential(*[m if isinstance(m, ReversibleModule) else
                                           ReversibleModule(m,
                                                            coupling_forward[i % len(coupling_forward)],
-                                                           coupling_inverse[i % len(coupling_inverse)],
-                                                           move_module_to_gpu,
-                                                           memory_savings)
+                                                           coupling_inverse[i % len(coupling_inverse)])
                                           for i, m in enumerate(modules)])
         self.split_dim = split_dim
 
