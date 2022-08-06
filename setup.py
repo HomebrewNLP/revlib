34dad19318e2f861ea6b0ce263506625a934b568
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-08-06 20:46:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     name='revlib',
     license='BSD',
     description='Simple and efficient RevNet-Library for PyTorch with XLA and DeepSpeed support and parameter offload',
-    version='1.7.1',
+    version='1.7.2',
     long_description=README,
     url='https://github.com/clashluke/revlib',
     packages=setuptools.find_packages(),
